270e0943a84489cb9c633a50d7132e41686ba838
Paweł Gorczyński <pawel.gorczynski@devfactory.com>
2018-05-12 12:07:45 +0200

    further works

--- SwaggerPhun/SwaggerPhun/Controllers/FileCommentController.cs
+++ SwaggerPhun/SwaggerPhun/Controllers/FileCommentController.cs
@@ -9,11 +9,12 @@ namespace SwaggerPhun.Controllers
     /// <response code="400">If arguments are missing or invalid</response> 
     /// <response code="401">If the authentication header is missing or user was not authenticated</response> 
     /// <response code="500">If a server-side error occured</response> 
-    [Route("api/[controller]")]
+    [Route("[controller]")]
 
     [ProducesResponseType(200)]
     [ProducesResponseType(typeof(ErrorDescriptor), 400)]
     [ProducesResponseType(typeof(ErrorDescriptor), 401)]
+    [ProducesResponseType(typeof(ErrorDescriptor), 403)]
     [ProducesResponseType(typeof(ErrorDescriptor), 500)]
     public class FileCommentController : Controller
     {
@@ -24,12 +25,13 @@ namespace SwaggerPhun.Controllers
         /// <returns></returns>
         /// <response code="400">If arguments are missing or invalid</response> 
         /// <response code="401">If the authentication header is missing or user was not authenticated</response> 
+        /// <response code="403">When the user has no rights to see the file</response> 
         /// <response code="500">If a server-side error occured</response> 
-        [HttpGet]
-        [ProducesResponseType(typeof(FileCommentDto[]), 200)]
-        public FileCommentDto[] Get(int fileId)
+        [HttpGet("{fileId}")]
+        [ProducesResponseType(typeof(FileCommentCollectionDto), 200)]
+        public FileCommentCollectionDto Get([FromRoute]int fileId)
         {
-            return new FileCommentDto[0];
+            return new FileCommentCollectionDto();
         }
 
         /// <summary>
@@ -41,10 +43,11 @@ namespace SwaggerPhun.Controllers
         /// <response code="201">When file was created successfully</response> 
         /// <response code="400">If arguments are missing or invalid</response> 
         /// <response code="401">If the authentication header is missing or user was not authenticated</response> 
+        /// <response code="403">When the user has no rights to see or comment the file</response> 
         /// <response code="500">If a server-side error occured</response> 
         [HttpPost("{fileId}")]
         [ProducesResponseType(201)]
-        public int Post(int fileId, [FromBody] FileCommentDto commentDto)
+        public int Post([FromRoute]int fileId, [FromBody] FileCommentDto commentDto)
         {
             return 1;
         }
@@ -57,10 +60,10 @@ namespace SwaggerPhun.Controllers
         /// <response code="403">When the invoking user in not the author of the comment</response> 
         /// <response code="400">If arguments are missing or invalid</response> 
         /// <response code="401">If the authentication header is missing or user was not authenticated</response> 
+        /// <response code="403">When the user has no rights to see or comment the file or is not the comment author</response> 
         /// <response code="500">If a server-side error occured</response> 
         [HttpPut("{commentId}")]
-        [ProducesResponseType(typeof(ErrorDescriptor), 403)]
-        public void Put(int commentId, [FromBody] FileCommentDto commentDto)
+        public void Put([FromRoute]int commentId, [FromBody] FileCommentDto commentDto)
         {
         }
 
@@ -68,15 +71,24 @@ namespace SwaggerPhun.Controllers
         /// Deletes an existing comment
         /// </summary>
         /// <param name="commentId"></param>
-        /// <response code="403">When the invoking user in not the author of the comment</response> 
         /// <response code="400">If arguments are missing or invalid</response> 
         /// <response code="401">If the authentication header is missing or user was not authenticated</response> 
+        /// <response code="403">When the user has no rights to see or comment the file</response> 
         /// <response code="500">If a server-side error occured</response> 
         [HttpDelete("{commentId}")]
         [ProducesResponseType(typeof(ErrorDescriptor), 403)]
-        public void Delete(int commentId)
+        public void Delete([FromRoute]int commentId)
         {
         }
+    }
+
+    public class FileCommentCollectionDto
+    {
+        public string SelfLink { get; set; }
+
+        public string Kind { get; set; }
+
+        public FileCommentDto[] Contents { get; set; }
     }
 
     /// <summary>
@@ -87,7 +99,7 @@ namespace SwaggerPhun.Controllers
         /// <summary>
         /// The content text of the comment
         /// </summary>
-        [Required] public string Contents { get; set; }
+        [Required] public string Text { get; set; }
     }
 
     /// <summary>
